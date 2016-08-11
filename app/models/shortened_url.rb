6491b5d29753179836407b28d18ee90ff8885f94
Dave Charnuska <davecharnuska@gmail.com>
2016-08-11 09:10:23 -0400

    add associations  to ShortenedUrl, User, and Visit

--- app/models/shortened_url.rb
+++ app/models/shortened_url.rb
@@ -1,8 +1,20 @@
 class ShortenedUrl < ActiveRecord::Base
-  validates :short_url, :long_url, :user_id :presence => true
+  validates :short_url, :long_url, :user_id, :presence => true
   validates :short_url, :uniqueness => true
 
   belongs_to :submitter, :class_name => "User", :foreign_key => :user_id, :primary_key => :id
+
+  has_many :visits,
+    :class_name => "Visit",
+    :foreign_key => :shortened_url_id,
+    :primary_key => :id
+
+  has_many(
+    :visitors,
+    # -> { distinct },
+    through: :visits,
+    source: :visitor
+  )
 
   def self.random_code
       loop do
--- app/models/visit.rb
+++ app/models/visit.rb
@@ -2,4 +2,14 @@ class Visit < ActiveRecord::Base
   def self.record_visit!(user, shortened_url)
     Visit.create(:user_id => user.id, :shortened_url_id => shortened_url.id)
   end
+
+  belongs_to :visitor,
+    :class_name => "User",
+    :primary_key => :id,
+    :foreign_key => :user_id
+
+  belongs_to :shortened_url,
+    :class_name => "ShortenedUrl",
+    :primary_key => :id,
+    :foreign_key => :shortened_url_id
 end
