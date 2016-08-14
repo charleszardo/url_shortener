b98a0427830b5a56f19600f98c62a943e3f4c9e6
Dave Charnuska <davecharnuska@gmail.com>
2016-08-14 15:08:37 -0400

    add TagTopic::most_popular_tags

--- app/models/tag_topic.rb
+++ app/models/tag_topic.rb
@@ -4,4 +4,13 @@ class TagTopic < ActiveRecord::Base
   has_many :taggings
 
   has_many :submitted_urls, :through => :taggings, :source => :shortened_url
+
+  def self.most_popular_tags
+    TagTopic
+      .select("tag_topics.*")
+      .joins("LEFT OUTER JOIN taggings ON taggings.tag_topic_id = tag_topics.id")
+      .group("tag_topics.topic")
+      .order("taggings.shortened_url_id DESC")
+      .limit(5)
+  end
 end
