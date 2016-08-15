5afa1ad98e94acfe5fc76cd83f485c8209157645
Dave Charnuska <davecharnuska@gmail.com>
2016-08-15 11:19:15 -0400

    add custom validations to Vote

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,7 +1,23 @@
 class Vote < ActiveRecord::Base
   validates :upvote, :shortened_url_id, :user_id, :presence => true
+  validate :user_cannot_vote_own_url
+  validate :limit_one_vote_per_url_per_user
 
   belongs_to :user
 
   belongs_to :shortened_url
+
+  def user_cannot_vote_own_url
+    if shortened_url.submitter_id == self.user_id
+      errors[:base] << "you cannot vote for your own url"
+    end
+  end
+
+  def limit_one_vote_per_url_per_user
+    vote = Vote.find(user_id: self.user_id, shortend_url_id: self.shortened_url_id)
+
+    if vote && vote.upvote == self.upvote
+      errors[:base] << "you can only vote once per url"
+    end
+  end
 end
